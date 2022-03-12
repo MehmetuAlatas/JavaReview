ce9c71a5e3ed61de5cce61addae24ea244b320d4
MehmetuAlatas <101453623+MehmetuAlatas@users.noreply.github.com>
2022-03-12 15:32:00 +0300

    Test
    testing

---
+++ Motorized.java
@@ -0,0 +1,9 @@
+package day23inheritance;
+
+public class Motorized extends Vehicle{
+
+    public Motorized(int capacity){
+    // super("String note"); this time Vehicle constructor with String parameter will execute
+        System.out.println("motorized constructor with parameter");
+    }
+}
